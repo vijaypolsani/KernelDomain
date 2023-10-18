48be156c8dc0348b8f68751e177cf0413f95a2a1
Vijay Polsani <vijay.polsani@gmail.com>
2023-10-18 12:01:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      "images.pexels.com",
-      "img.freepik.com",
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-    ],
-  },
-};
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-module.exports = nextConfig;
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+}
